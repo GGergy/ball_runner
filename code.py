2548d00016e8fa03a3020f1756c12fcedbf21762
GGergy <101131722+GGergy@users.noreply.github.com>
2022-07-18 00:16:59 +0300

    Update code.py
    statistic

--- code.py
+++ code.py
@@ -1,21 +1,19 @@
 import pygame
 import random
 import os.path
+from openpyxl import Workbook, load_workbook
 from time import sleep
 from timeit import default_timer
-
-if not os.path.isfile('high2.txt'):
-    with open('high2.txt', 'w') as file:
-        file.write('0')
 if not os.path.isfile('skin.txt'):
     with open('skin.txt', 'w') as file:
         file.write('ball1.png')
-
+if not os.path.isfile('userdata.xlsx'):
+    wb = Workbook()
+    wb.save('userdata.xlsx')
 pygame.init()
 screen_width = pygame.display.Info().current_w
 screen_height = pygame.display.Info().current_h
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.font.init()
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 BROWN = (150, 75, 0)
@@ -24,20 +22,21 @@ menu = True
 username = False
 clock =  pygame.time.Clock()
 wl = {'main2.mp3': (14.3, 30, 'Live Another Day'), 'main3.mp3': (17, 45, 'SiLence'), 'main4.mp3': (11.4, 40, 'Devil Eyes'),
-'main5.mp3': (13.5, 30, 'TwiLight'), 'main6.mp3': (6.5, 27, 'DynamIc'), 'main7.mp3': (13, 30, 'Prince of Darkness'), 'main8.mp3': (0, 25, 'OverRide')}
+'main5.mp3': (13.5, 30, 'TwiLight'), 'main6.mp3': (6.5, 27, 'DynamIc'), 'main7.mp3': (13, 30, 'Prince of Darkness'), 'main8.mp3': (0, 20, 'OverRide'),
+'main9.mp3': (11.5, 18, 'MidNight'), 'main10.mp3': (8.2, 20, 'DisasteR')}
 skins = ['ball1.png', 'ball2.png', 'ball3.png', 'ball4.png', 'ball5.png']
-with open('high2.txt', 'r') as file:
-        game_high = int(file.read())
 with open('skin.txt', 'r') as file:
         ball_skin = file.read()
-FPS = 60
-jump_sound = pygame.mixer.Sound('jump.wav')
+game_high = 0
+FPS = 144
+jump_sound = pygame.mixer.Sound('jump.mp3')
 count_sound = pygame.mixer.Sound('countdown.mp3')
 start_sound = pygame.mixer.Sound('final.mp3')
 q_sound = pygame.mixer.Sound('q_tap.mp3')
 fail_sound = pygame.mixer.Sound('fail.mp3')
+game_user = None
 def restart():
-    global chunks, emp, game_speed, game_score, game_hb, in_phonk, bsp, j, did, jr, ball_img, zero_d, sun_color, main_color, colorlist, game_cheats, main_sound, main_song_name, ball_skin, wl, circles, colorlist, username
+    global chunks, emp, game_speed, game_score, game_high, game_hb, in_phonk, bsp, j, did, jr, ball_img, zero_d, sun_color, main_color, colorlist, game_cheats, main_sound, main_song_name, ball_skin, wl, circles, colorlist, game_user, time0
     game_speed = 4 * (60 / FPS)
     game_score = 0
     bsp = 1
@@ -60,13 +59,17 @@ def restart():
             pr_text(txt='select song', n=50, cord=(screen_width // 2, 50))
             pr_text(txt='Select skin', cord=(150, 75), n=30)
             pr_text(txt='Login', cord=(screen_width - 150, 75), n=30)
-            if username:
-                pr_text(txt=f'Hi, {username}', cord=(screen_width - 150, 150), n=30)
+            if game_user:
+                pr_text(txt='Statistic', cord=(screen_width - 150, 200), n=30)
+            if game_user:
+                pr_text(txt=f'Hi, {game_user.name}', cord=(screen_width - 150, 150), n=30)
             y = 140
             i = 1
             for elem in wl:
                 nmb = pr_text(txt=str(i) + '. ' + wl[elem][2], n=30, cord=(screen_width // 2, y), color=(253,149,253), frmt=True)
                 buttons.append(Button(nmb.topleft[0] - 5, nmb.topleft[1]- 5, nmb.width + 10, nmb.height + 10, type='song', song_id=i + 1))
+                if game_user:
+                    buttons.append(Button(screen_width - 210, 175, 120, 50, 'stat'))
                 pr_text(txt=str(i) + '. ' + wl[elem][2], n=30, cord=(screen_width // 2, y), color=(253,149,253))
                 y += 75
                 i += 1
@@ -81,7 +84,7 @@ def restart():
             for button in buttons:
                 button.is_click()
             for event in events:
-                if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     for button in buttons:
                         if button.is_click():
                             if button.type == 'skin':
@@ -90,8 +93,20 @@ def restart():
                                 draw()
                             elif button.type == 'song' and not button_invisible:
                                 main_song_name = 'main' + str(button.song_id) + '.mp3'
+                                if game_user:
+                                    game_user.songs[button.song_id] += 1
+                                continue
                             elif button.type == 'account':
                                 username = enter_on_keyboard()
+                                data = load_workbook('userdata.xlsx')
+                                game_user = User(username, data)
+                                game_user.get_info()
+                                game_high = game_user.high
+                                time0 = default_timer()
+                                button_invisible = True
+                                draw()
+                            elif button.type == 'stat':
+                                statistic()
                                 button_invisible = True
                                 draw()
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE or event.type == pygame.QUIT:
@@ -114,14 +129,17 @@ def restart():
                         pygame.display.update()
                         events = pygame.event.get()
                         for event in events:
-                            if event.type == pygame.MOUSEBUTTONDOWN:
+                            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                                 if da.is_click():
                                     pygame.mixer.Sound('pora.mp3').play()
+                                    if game_user:
+                                        time1 = default_timer()
+                                        game_user.time += int(time1 - time0)
+                                        game_user.upload_info()
                                     sleep(2)
                                     exit()
                                 if net.is_click():
                                     pygame.mixer.Sound('posidim.mp3').play()
-                                    sleep(2)
                                     exit_usl = False
                                     break
                     button_invisible = True
@@ -158,20 +176,13 @@ def restart():
     pygame.draw.rect(screen, BLACK, (0, 0, screen_width, screen_height))
     ball_img = pygame.image.load(ball_skin)
     zero_d = default_timer()
+    if game_user:
+        game_user.games += 1
     main_sound.play()
 
 
-def draw_background():
-    pygame.draw.rect(screen, main_color, (0, 0, screen_width, screen_height))
-    pygame.draw.circle(screen, sun_color, (screen_width, 0), 100)
-    if in_phonk:
-        if default_timer() - zero_d <= main_sound.get_length():
-            circle_spawn()
-        for circle in circles:
-            circle.move()
-
 def change_skin():
-    global ball_skin
+    global ball_skin, game_user
     pygame.draw.rect(screen, (23,18,48), (0, 0, screen_width, screen_height))
     x = screen_width // 2 - len(skins) // 2 * 120
     i = 1
@@ -196,48 +207,13 @@ def change_skin():
                 for b in skin_buttons:
                         if b.is_click():
                             ball_skin = 'ball' + str(b.song_id) + '.png'
+                            if game_user:
+                                game_user.skins[b.song_id] += 1
             elif event.type == pygame.QUIT:
                 exit()
     with open('skin.txt', 'w') as file:
                 file.write(ball_skin)
 
-def circle_spawn(crcls=screen_width * screen_height // 30000):
-    for i in range(crcls):
-        if i >= len(circles):
-            break
-        if circles[i].stade > circles[i].nstade:
-            del circles[i]
-    while len(circles) < crcls:
-        circles.append(Circles())
-
-def rotate(screen, image, angle):
-
-    rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(center=(emp.x, emp.y))
-
-    screen.blit(rotated_image, new_rect)
-
-def moving_chunks(chunks):
-    for ch in chunks:
-        if ch:
-            ch.chunk_move()
-
-def pr_text(txt, cord=(320, 240), n=20, color=(255, 255, 255), ctr=True, frmt=False):
-    font = pygame.font.SysFont('arial', n)
-    text = font.render(txt, True, color)
-    if frmt:
-        return text.get_rect(center=cord)
-    if ctr:
-        text_rect = text.get_rect(center=cord)
-        screen.blit(text, text_rect)
-    else:
-        screen.blit(text, cord)
-
-
-def punch(emp, chunk):
-    if chunk.sp and chunk.rect.collidepoint(emp.x, emp.y):
-        return game_hb
-    return False 
 
 def chunk_generated(i=0):
     while chunks[-1].sp_x < screen_width:
@@ -249,6 +225,27 @@ def chunk_generated(i=0):
     for i in range(len(chunks)):
         if chunks[i] and chunks[i].sp_x <= 0 - chunks[i].ch_width:
             chunks[i] = None
+
+
+def circle_spawn(crcls=screen_width * screen_height // 30000):
+    for i in range(crcls):
+        if i >= len(circles):
+            break
+        if circles[i].stade > circles[i].nstade:
+            del circles[i]
+    for i in range(len(circles), crcls):
+        circles.append(Circles())
+
+
+def draw_background():
+    pygame.draw.rect(screen, main_color, (0, 0, screen_width, screen_height))
+    pygame.draw.circle(screen, sun_color, (screen_width, 0), 100)
+    if in_phonk:
+        if default_timer() - zero_d <= main_sound.get_length():
+            circle_spawn()
+        for circle in circles:
+            circle.move()
+
 
 def enter_on_keyboard(c=(screen_width // 2, screen_height // 2)):
     name = ''
@@ -306,16 +303,11 @@ def enter_on_keyboard(c=(screen_width // 2, screen_height // 2)):
             butt.light(True)
             pygame.display.update()
     return name
-        
 
-def switch_hb():
-    global game_hb, game_cheats
-    game_hb = not game_hb
-    game_cheats = True
 
 def phonk():
     global in_phonk, colorlist, sun_color, chunks, main_color, circles, game_speed
-    game_speed = 12 * (60 / FPS)
+    game_speed = 10 * (60 / FPS)
     in_phonk = True
     colorlist = [(128,0,128), (89,0,128), (140,0,128), (50,0,128), (113,0,128), (36,0,128), (61,0,128)]
     sun_color = (189,208,228)
@@ -330,7 +322,56 @@ def phonk():
     chunk_generated(4)
     sleep(2)
 
- 
+
+def pr_text(txt, cord=(320, 240), n=20, color=(255, 255, 255), ctr=True, frmt=False):
+    font = pygame.font.SysFont('arial', n)
+    text = font.render(txt, True, color)
+    if frmt:
+        return text.get_rect(center=cord)
+    if ctr:
+        text_rect = text.get_rect(center=cord)
+        screen.blit(text, text_rect)
+    else:
+        screen.blit(text, cord)
+
+
+def punch(emp, chunk):
+    if chunk.sp and chunk.rect.collidepoint(emp.x, emp.y):
+        return game_hb
+    return False 
+
+
+def rotate(screen, image, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=(emp.x, emp.y))
+    screen.blit(rotated_image, new_rect)
+
+
+def statistic():
+    def get_key(d, value):
+        for k, v in d.items():
+            if v == value:
+                return k
+    key = 'main' + str(get_key(game_user.songs, max(game_user.songs.values()))) + '.mp3'
+    pygame.draw.rect(screen, (23,18,48), (0, 0, screen_width, screen_height))
+    points = (f'Nickname: {game_user.name}', f'Games played: {game_user.games}', f'Time in game: {game_user.time // 3600}h {game_user.time % 3600 // 60}m {game_user.time % 60}s',
+    f'Favourite song: {wl[key][2]}', 'Favourite skin:')
+    x = 150
+    y = 300
+    pr_text('Your Statistics:', cord=(screen_width // 2, 100), n=50)
+    for i in range(len(points)):
+        pr_text(points[i], (x, y), n=35, ctr=False)
+        y += 50
+    key = 'ball' + str(get_key(game_user.skins, max(game_user.skins.values()))) + '.png'
+    img = pygame.image.load(key)
+    screen.blit(img, img.get_rect(center=(400, 520)))
+    pygame.display.update()
+    f = True
+    while f:
+        for e in pygame.event.get():
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                f = False
+
 class Empty:
     def __init__(self, x, y, rad, col) -> None:
         self.x = x
@@ -403,8 +444,8 @@ class Circles:
     def __init__(self):
         self.rad = 0
         self.maxrad = 75
+        self.nstade = int(wl[main_song_name][1] * (FPS / 60))
         self.stade = 1
-        self.nstade = wl[main_song_name][1] * (FPS / 60)
         self.y = random.randint(0, screen_height)
         self.x = random.randint(0, screen_width)
         self.color = random.choice(colorlist)
@@ -445,8 +486,51 @@ class Button:
             pygame.display.update()
             self.last_light = show
 
-restart()
 
+class User:
+    def __init__(self, name, doc) -> None:
+        self.name = name
+        self.doc = doc
+    
+    def get_info(self):
+        self.high = 0
+        self.games = 0
+        self.time = 0
+        self.songs = {}
+        self.skins = {}
+        if self.name not in self.doc.sheetnames:
+            self.doc.create_sheet(self.name)
+            sheet = self.doc[self.name]
+            for i in range(1, 4):
+                sheet['A' + str(i)].value = 0
+            for i in range(1, len(wl) + 1):
+                sheet['B' + str(i)].value = 0
+                self.songs[i + 1] = 0
+            for i in range(1, len(skins) + 1):
+                sheet['C' + str(i)].value = 0
+                self.skins[i] = 0
+        else:
+            sheet = self.doc[self.name]
+            self.high = sheet['A1'].value
+            self.games = sheet['A2'].value
+            self.time = sheet['A3'].value
+            for i in range(1, len(wl) + 1):
+                self.songs[i + 1] = sheet['B' + str(i)].value
+            for i in range(1, len(skins) + 1):
+                self.skins[i] = sheet['C' + str(i)].value
+
+    def upload_info(self):
+        sheet = self.doc[self.name]
+        sheet['A1'].value = self.high
+        sheet['A2'].value = self.games
+        sheet['A3'].value = self.time
+        for i in range(1, len(wl) + 1):
+            sheet['B' + str(i)].value = self.songs[i + 1] = 0
+        for i in range(1, len(skins) + 1):
+            sheet['C' + str(i)].value = self.skins[i] = 0
+        self.doc.save('userdata.xlsx')
+
+restart()
 while True: 
     #print(default_timer() - zero_d)
     if default_timer() - zero_d >= wl[main_song_name][0] and not in_phonk:
@@ -464,10 +548,10 @@ while True:
                 game_score -= 1
             if game_score > game_high and not game_cheats:
                 game_high = game_score
-                with open('high2.txt', 'w') as file:
-                    file.write(str(game_high))
+                if game_user:
+                    game_user.high = game_high
 
-            pr_text(f'game over, score - {game_score}  hight - {game_high}', n=36, cord=(screen_width // 2, 130))
+            pr_text(f'game over, score: {game_score}  hight: {game_high}', n=36, cord=(screen_width // 2, 130))
             pr_text('CTRL to restart, ESC to exit in main menu', n=30, cord=(screen_width // 2, 200))
 
             pygame.display.update()
@@ -508,7 +592,8 @@ while True:
                 else:
                     upd = -1
             if event.key == pygame.K_h: 
-                switch_hb()
+                game_hb = not game_hb
+                game_cheats = True
             if event.key == pygame.K_ESCAPE:
                 start = default_timer()
                 pygame.mixer.pause()
@@ -549,12 +634,13 @@ while True:
         q_sound.play()
         emp.jump_reset(upd)
         jr = False
-    
-    chunk_generated()
+
     draw_background()
-    moving_chunks(chunks)
-    if game_speed < 6:
-        game_speed *= 1.0005
+    for ch in chunks:
+        if ch:
+            ch.chunk_move()
+    chunk_generated()
+
     rotate(screen, ball_img, -rot % 360)
     if not j:
         rot += 5 * (60 / FPS)
@@ -563,18 +649,19 @@ while True:
     if default_timer() - zero_d >= main_sound.get_length() // 1 + 2:
         if game_score > game_high and not game_cheats:
             game_high = game_score
-            with open('high2.txt', 'w') as file:
-                file.write(str(game_high))
-        pr_text(txt=f'complete! score - {game_score}', n=70, cord=(screen_width // 2, screen_height // 2 - 100), color=(1,112,34))
+            if game_user:
+                game_user.high = game_high
+        pr_text(txt=f'complete! score: {game_score}', n=70, cord=(screen_width // 2, screen_height // 2 - 100), color=(1,112,34))
         menu = True
         pygame.display.update()
         sleep(3)
         restart()
         pygame.display.update()
         continue
-    pr_text(f'this-{game_score} high-{game_high}', cord=(50, 50), ctr=False)
-    if username:
-        pr_text(f'playing - {username}', cord=(screen_width - 250, 50), ctr=False)
+    txt = f'this-{game_score}' + (f'high-{game_high}' if game_user else '  log in to see statistic')
+    pr_text(txt, cord=(50, 50), ctr=False)
+    if game_user:
+        pr_text(f'playing - {game_user.name}', cord=(screen_width - 250, 50), ctr=False)
     if not game_hb:
         pr_text('!GOD MODE!', cord=(screen_width // 2, 50), n=40, color=(227,20,20))
     pygame.display.update()
